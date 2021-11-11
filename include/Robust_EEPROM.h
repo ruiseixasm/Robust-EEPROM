d256b40cb242c2c95e1e7ce7888bec282c46c69f
ruiseixasm <ruiseixas@yahoo.com>
2021-11-11 10:21:47 +0000

    Readme

--- include/Robust_EEPROM.h
+++ include/Robust_EEPROM.h
@@ -1,3 +1,15 @@
+/*
+  Robust_EEPROM.h - Robust_EEPROM library
+  Original Copyright (c) 2021 Rui Seixas Monteiro. All right reserved.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+*/
 #ifndef Robust_EEPROM_H_INCLUDED
 #define Robust_EEPROM_H_INCLUDED
 
