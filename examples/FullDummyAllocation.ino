bcf196063cf9349125e7833962de6b074c0b52c4
ruiseixasm <ruiseixas@yahoo.com>
2022-02-05 03:04:55 +0000

    ExamplesUpdated

--- examples/FullDummyAllocation.ino
+++ examples/FullDummyAllocation.ino
@@ -1,10 +1,13 @@
 #include <Robust_EEPROM.h>
 
+int test_array_length = 10;
+int *starting_data_array;  
 int length = 0;
 int data_health = 0;
-bool stop = false;
 Dummy_EEPROM *dummy_eeprom;
 Robust_EEPROM *robust_eeprom;
+enum Test {testing, result, stop};
+Test test = testing;
 
 void setup() {
     Serial.begin(9600);
@@ -16,18 +19,21 @@ void setup() {
 
     Serial.println("Starting!");
     Serial.println("");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < test_array_length; i++)
         robust_eeprom->update(i, rand() % 256);
-    }
+        
+    starting_data_array = new int[test_array_length];
+    for (int i = 0; i < test_array_length; i++)
+        starting_data_array[i] = robust_eeprom->read(i);
+
 }
 
 void loop() {
 
-    if (!stop) {
+    if (test == testing) {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < test_array_length / 2; i++)
             robust_eeprom->update(i, rand() % 256);
-        }
     
         if (length != robust_eeprom->length()) {
 
@@ -35,21 +41,21 @@ void loop() {
 
             Serial.println("Virtual Addresses");
             Serial.print("    ");
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < test_array_length; i++) {
                 Serial.print(i);
                 Serial.print(":");
             }
             Serial.println("");
             Serial.println("Physical Addresses");
             Serial.print("    ");
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < test_array_length; i++) {
                 Serial.print(robust_eeprom->physicalbyte(i));
                 Serial.print(":");
             }
             Serial.println("");
             Serial.println("Data");
             Serial.print("    ");
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < test_array_length; i++) {
                 Serial.print(robust_eeprom->read(i));
                 Serial.print(":");
             }
@@ -66,13 +72,28 @@ void loop() {
             Serial.println("%");
             Serial.println("");
             if (data_health < 20) {  // Last Percentage %
-                stop = true;
+                test = result;
                 Serial.println("Finish!");
             }
-            
             length = robust_eeprom->length();
         }
         
-    }
+    } else if (test == result) {
 
+        // Check memory integrity
+        bool passed = true;
+        for (int i = test_array_length / 2; i < test_array_length; i++)
+            if (starting_data_array[i] != robust_eeprom->read(i)) {
+                passed = false;
+                break;
+            }
+
+        Serial.println("---------------------------------------");
+        if (passed)
+            Serial.println("PASSED!");
+        else
+            Serial.println("FAIL!");
+                
+        test = stop;
+    }
 }
